ac9091a341907003565f3e2437bb91a248719d62
ReemMohamedAbdelfatah <reem.mohamed.abdelfatah@gmail.com>
2022-09-17 10:48:42 +0200

    Add local storage

--- script.js
+++ script.js
@@ -38,3 +38,30 @@ form.addEventListener('submit', (e) => {
     email.style.border = '1px solid red';
   }
 });
+
+const localData = localStorage.getItem('info');
+const userData = JSON.parse(localData);
+
+const userInfo = localData ? JSON.parse(localData) : { name: '', email: '', text: '' };
+const { name, emaill, text } = form.elements;
+if (userData) {
+  name.value = userData.name;
+  emaill.value = userData.user_email;
+  text.value = userData.message;
+}
+
+form.addEventListener('change', (e) => {
+  e.preventDefault();
+  if (e.target.name === 'name') {
+    userInfo.name = e.target.value;
+  }
+  if (e.target.name === 'user_email') {
+    userInfo.email = e.target.value;
+  }
+  if (e.target.name === 'message') {
+    userInfo.text = e.target.value;
+  }
+
+  const localStore = JSON.stringify(userInfo);
+  localStorage.setItem('info', localStore);
+});
